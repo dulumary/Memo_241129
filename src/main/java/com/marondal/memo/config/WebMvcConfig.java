397f8373ec0d392f55e3bd8a91d199fc98998517
dulumary <lecture@hagulu.com>
2024-12-17 16:32:40 +0900

    [김인규]  - interceptor 추가

--- src/main/java/com/marondal/memo/config/WebMvcConfig.java
+++ src/main/java/com/marondal/memo/config/WebMvcConfig.java
@@ -1,10 +1,12 @@
 package com.marondal.memo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.marondal.memo.common.FileManager;
+import com.marondal.memo.interceptor.PermissionInterceptor;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -15,4 +17,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		.addResourceLocations("file:///" + FileManager.FILE_UPLOAD_PATH + "/");
 	}
 
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		
+		registry.addInterceptor(new PermissionInterceptor())
+		.addPathPatterns("/**")
+		.excludePathPatterns("/user/logout", "/static/**", "/images/**");
+		
+		
+		
+	}
 }
